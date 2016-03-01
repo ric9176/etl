fa2ef5f937ecd1fb65bedf7e646fde7b9a3b7b17
charlieperson <charlie.person@hyperisland.se>
2016-03-01 17:59:03 +0000

    Adds link to homepage

--- spec/features/info_feature_spec.rb
+++ spec/features/info_feature_spec.rb
@@ -5,6 +5,7 @@ feature 'info' do
     scenario 'should show information about ETL' do
       visit '/'
       expect(page).to have_content 'Can You Really Improve Your English From Home?'
+      expect(page).to have_link 'WHY US'
     end
   end
 end
