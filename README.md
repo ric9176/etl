0fa3e701136fc18da85eaa5c3604baa3a5363f37
Richard Moss <richard@englishtutorslive.com>
2016-03-02 13:00:11 +0000

    adds passing test to test moving from one static page to another

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# etl
-
 [![Build Status](https://travis-ci.org/ric9176/etl.svg?branch=master)](https://travis-ci.org/ric9176/etl)
+# etl
--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -7,5 +7,11 @@ feature 'static_pages' do
       expect(page).to have_content 'Can You Really Improve Your English From Home?'
       expect(page).to have_link 'WHY US'
     end
+
+    scenario 'should allow users to link to other pages' do
+      visit '/static_pages/home'
+      click_link('WHY US')
+      expect(current_path).to eq('/static_pages/why_us')
+    end
   end
 end
