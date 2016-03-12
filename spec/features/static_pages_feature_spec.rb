56ec1d38ee80d2f6c11ba37e527ce28788f5f3db
Richard Moss <richard@englishtutorslive.com>
2016-03-12 16:11:10 +0000

    adds navbar links into layouts and updates static page spec

--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 feature 'static_pages' do
   context 'first time user visits site' do
-    scenario 'should show information about ETL' do
+    scenario 'should show information about ETL and have navagation bar links' do
       visit '/'
       expect(page).to have_content 'Can You Really Improve Your English From Home?'
-      expect(page).to have_link 'WHY US'
+      expect(page).to have_link 'Why Us'
+      expect(page).to have_link 'Services'
+      expect(page).to have_link 'Pricing'
+      expect(page).to have_link 'Book a Free Lesson'
+      expect(page).to have_link 'Contact'
+      expect(page).to have_link 'Blog'
     end
 
     scenario 'should allow users to link to other pages' do
       visit '/'
-      click_link('WHY US')
+      click_link('Why Us')
       expect(current_path).to eq('/why_us')
     end
   end
