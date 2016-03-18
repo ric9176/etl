81e642aadf5a4cf1c76a61090e6f8fb0f9e0c8dd
charlieperson <charlie.person@hyperisland.se>
2016-03-18 13:46:42 +0000

    Passes all tests except for users being directed to their specific profile because database cleaner is not yet working

--- spec/features/teacher_profile_spec.rb
+++ spec/features/teacher_profile_spec.rb
@@ -3,24 +3,23 @@ require 'rails_helper'
 feature 'teacher_profiles' do
   context 'new teacher signs up' do
     scenario 'once teacher logs in, they should be able to view/edit their profile' do
-      visit '/users/sign_up'
-      fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'testtest'
-      fill_in 'Password confirmation', with: 'testtest'
-      click_button 'Sign up'
+      sign_up('test@test.com', 'testtest', 'testtest')
       expect(current_path).to eq '/dashboard'
       expect(page).to have_link 'My profile'
     end
 
     scenario 'teacher can click link to see their profile which takes them to their profile' do
-      visit '/users/sign_up'
-      fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'testtest'
-      fill_in 'Password confirmation', with: 'testtest'
-      click_button 'Sign up'
+      sign_up('test@test.com', 'testtest', 'testtest')
+      expect(page).to have_content('Welcome, test@test.com')
+      expect(page).to have_link 'My profile'
+    end
+
+    scenario 'users are directed to their specific profile page' do
+      sign_up('test@test.com', 'testtest', 'testtest')
+      click_link 'Log out'
+      sign_up('test2@test.com', 'testtest', 'testtest')
       click_link 'My profile'
-      expect(page).to have_content("test@test.com's profile")
-      expect(page).to have_link 'Edit profile'
+      expect(current_path).to eq '/profiles/2'
     end
   end
 
