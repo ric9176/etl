ee071d9bc5be13f15b91b1ed76158cdf2b5b6f74
charlieperson <charlie.person@hyperisland.se>
2016-03-19 09:49:58 +0000

    Allows user to create a profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,29 @@
 class ProfilesController < ApplicationController
 
-  def show
+  def index
     @user = current_user
+  end
+
+  def create
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @current_user = current_user
+    @profile = Profile.create(profile_params)
+    @current_user.profile = @profile
+    if @profile.save
+      redirect_to '/profiles'
+    else
+      render 'new'
+    end
+  end
+
+
+  def profile_params
+    params.require(:profile).permit(:name, :bio)
   end
 end
