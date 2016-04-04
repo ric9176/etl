b8eb3b5e091b0095fff2fefdfecd6b13559044b0
charlieperson <charlie.person@hyperisland.se>
2016-04-04 14:14:42 +0100

    Adds dashboard welcome message test

--- spec/features/student_dashboard_spec.rb
+++ spec/features/student_dashboard_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 feature 'student_dashboards' do
   context 'new student signs up' do
-
     scenario 'once a student logs in, they should be able to view/edit their profile' do
       sign_up('test@test.com', 'testtest', 'testtest')
       expect(current_path).to eq '/dashboard'
       expect(page).to have_link 'My profile'
+    end
+    scenario 'Welcome message appears on a student dashboard' do
+      sign_up('test@test.com', 'testtest', 'testtest')
+      expect(page).to have_content("test@test.com's dashboard")
     end
   end
   context 'not signed in' do
