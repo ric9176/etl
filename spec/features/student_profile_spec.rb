b96be49fb657d2b281b07658024bcaa24703bc5e
charlieperson <charlie.person@hyperisland.se>
2016-04-04 13:46:15 +0100

    Adds student_dashboard_spec.rb with passing tests

--- spec/features/student_profile_spec.rb
+++ spec/features/student_profile_spec.rb
@@ -1,14 +1,9 @@
 require 'rails_helper'
 
-feature 'teacher_profiles' do
-  context 'new teacher signs up' do
-    scenario 'once teacher logs in, they should be able to view/edit their profile' do
-      sign_up('test@test.com', 'testtest', 'testtest')
-      expect(current_path).to eq '/dashboard'
-      expect(page).to have_link 'My profile'
-    end
+feature 'student_profiles' do
+  context 'new student signs up' do
 
-    scenario 'teacher can click link to see their profile which takes them to their profile' do
+    scenario 'a student can click link to see their profile which takes them to their profile' do
       sign_up('test@test.com', 'testtest', 'testtest')
       expect(page).to have_content('Welcome, test@test.com')
       expect(page).to have_link 'My profile'
@@ -24,7 +19,7 @@ feature 'teacher_profiles' do
   end
 
   context 'not signed up/in' do
-    scenario 'teachers cannot view profile when signed out' do
+    scenario 'students cannot view profile when signed out' do
       visit '/'
       expect(page).not_to have_link 'My profile'
     end
