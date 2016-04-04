c6ee4198279d50e350b718cdd3a2419f739bb36e
Richard Moss <richard@englishtutorslive.com>
2016-04-04 13:10:16 +0100

    updates rake version

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,16 @@ ActiveRecord::Schema.define(version: 20160404115334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "bio",        limit: 200
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
+
   create_table "students", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -34,7 +44,7 @@ ActiveRecord::Schema.define(version: 20160404115334) do
   add_index "students", ["email"], name: "index_students_on_email", unique: true, using: :btree
   add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true, using: :btree
 
-  create_table "students", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.boolean  "teacher"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -50,7 +60,8 @@ ActiveRecord::Schema.define(version: 20160404115334) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "students", ["email"], name: "index_students_on_email", unique: true, using: :btree
-  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "users"
 end
