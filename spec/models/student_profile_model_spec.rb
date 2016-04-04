2bee2ec60f4596cb07de55563db948b630f0634d
charlieperson <charlie.person@hyperisland.se>
2016-04-04 22:43:30 +0100

    Adds StudentProfile 1:1 association with Student, along with passing tests

--- spec/models/student_profile_model_spec.rb
+++ spec/models/student_profile_model_spec.rb
@@ -5,10 +5,19 @@ describe StudentProfile do
     expect(class_exists?("StudentProfile"))
     expect(StudentProfile < ActiveRecord::Base).to eq(true)
   end
-  context "Student class properties defined" do
+  context "StudentProfile class properties defined" do
     subject(:user) { StudentProfile.new }
     it { is_expected.to respond_to(:name) }
     it { is_expected.to respond_to(:native_language) }
     it { is_expected.to respond_to(:learning_objectives) }
+  end
+  context "StudentProfile associations" do
+    it "Profile 1:1 belongs_to relationship to Student in place" do
+      expect(StudentProfile.reflect_on_association(:student).macro).to eq :belongs_to
+    end
+
+    it "Student 1:1 has_one relationship to Profile in place" do
+      expect(Student.reflect_on_association(:student_profile).macro).to eq :has_one
+    end
   end
 end
