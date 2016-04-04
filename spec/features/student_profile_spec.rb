2c59759411f46c731219284090d0cb6004e35ef6
Richard Moss <richard@englishtutorslive.com>
2016-04-04 15:16:16 +0100

    creates failing feature test for current student signing in and view their profile

--- spec/features/student_profile_spec.rb
+++ spec/features/student_profile_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 feature 'student_profiles' do
   context 'new student signs up' do
-
     scenario 'a student can click link to see their profile which takes them to their profile' do
       sign_up('test@test.com', 'testtest', 'testtest')
       expect(page).to have_content('Welcome, test@test.com')
       expect(page).to have_link 'My profile'
+    end
+
+    scenario 'A new student is directed to create a profile' do
+      sign_up('test@test.com', 'testtest', 'testtest')
+      click_link 'My profile'
+      expect(page).to have_content 'You do not have a profile yet'
+      expect(page).to have_link 'Create Profile'
     end
 
     xscenario 'students are directed to their specific profile page' do
@@ -18,12 +24,24 @@ feature 'student_profiles' do
     end
   end
 
+  context 'current student with profile log in' do
+    before do
+      sign_up('test@test.com', 'testtest', 'testtest')
+      click_link 'Log out'
+      click_link 'Sign in'
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'testtest'
+      click_button 'Log in'
+    end
+    scenario 'student views their profile' do
+      expect(page).to have_content "I'm an amazing student from Spain"
+    end
+  end
+
   context 'not signed up/in' do
     scenario 'students cannot view profile when signed out' do
       visit '/'
       expect(page).not_to have_link 'My profile'
     end
   end
-
-
 end
