290e7eee3e9583c199dae1d5b6f06189ba64ba57
Richard Moss <richard@englishtutorslive.com>
2016-04-06 10:38:31 +0100

    further refactor of student profile spec, adds web helper method and cleans up

--- spec/features/student_profile_spec.rb
+++ spec/features/student_profile_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'student_profiles' do
   context 'new student signs up' do
     before do
-      sign_up('test@test.com', 'testtest', 'testtest')
+      sign_up
     end
     scenario 'a student can click link to see their profile which takes them to their profile' do
       expect(page).to have_content('Welcome, test@test.com')
@@ -19,25 +19,20 @@ feature 'student_profiles' do
 
   context 'Student is already signed up' do
     before do
-      sign_up('test@test.com', 'testtest', 'testtest')
-      click_link 'Log out'
-      click_link 'Sign in'
-      fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'testtest'
-      click_button 'Log in'
+      sign_up
+      log_out_then_sign_in
+    end
+
+    xscenario 'students are directed to their specific profile page' do
+      click_link 'My profile'
+      expect(current_path).to eq '/profiles/2'
     end
 
     scenario 'student views their profile' do
       expect(page).to have_content "I'm an amazing student from Spain"
       expect(page).to have_link 'Edit Profile'
     end
-
-    xscenario 'students are directed to their specific profile page' do
-    click_link 'My profile'
-    expect(current_path).to eq '/profiles/2'
-    end
   end
-
 
   context 'not signed up/in' do
     scenario 'students cannot view profile when signed out' do
