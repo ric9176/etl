88eef99199f38c23f854e1cc0d53f901ecb445fd
Richard Moss <richard@englishtutorslive.com>
2016-04-06 10:25:40 +0100

    refactors student_profile_spec

--- spec/features/student_profile_spec.rb
+++ spec/features/student_profile_spec.rb
@@ -2,29 +2,22 @@ require 'rails_helper'
 
 feature 'student_profiles' do
   context 'new student signs up' do
-    scenario 'a student can click link to see their profile which takes them to their profile' do
+    before do
       sign_up('test@test.com', 'testtest', 'testtest')
+    end
+    scenario 'a student can click link to see their profile which takes them to their profile' do
       expect(page).to have_content('Welcome, test@test.com')
       expect(page).to have_link 'My profile'
     end
 
     scenario 'A new student is directed to create a profile' do
-      sign_up('test@test.com', 'testtest', 'testtest')
       click_link 'My profile'
       expect(page).to have_content 'You do not have a profile yet'
       expect(page).to have_link 'Create Profile'
     end
-
-    xscenario 'students are directed to their specific profile page' do
-      sign_up('test@test.com', 'testtest', 'testtest')
-      click_link 'Log out'
-      sign_up('test2@test.com', 'testtest', 'testtest')
-      click_link 'My profile'
-      expect(current_path).to eq '/profiles/2'
-    end
   end
 
-  context 'current student with profile log in' do
+  context 'Student is already signed up' do
     before do
       sign_up('test@test.com', 'testtest', 'testtest')
       click_link 'Log out'
@@ -33,10 +26,17 @@ feature 'student_profiles' do
       fill_in 'Password', with: 'testtest'
       click_button 'Log in'
     end
+
     scenario 'student views their profile' do
       expect(page).to have_content "I'm an amazing student from Spain"
     end
+
+    xscenario 'students are directed to their specific profile page' do
+    click_link 'My profile'
+    expect(current_path).to eq '/profiles/2'
+    end
   end
+
 
   context 'not signed up/in' do
     scenario 'students cannot view profile when signed out' do
