686b529c3236c2916349f75d7a36abf0c273baca
charlieperson <charlie.person@hyperisland.se>
2016-04-06 23:44:01 +0100

    StudentProfiles can be created by students with passing tests

--- app/controllers/student_profiles_controller.rb
+++ app/controllers/student_profiles_controller.rb
@@ -1,2 +1,28 @@
 class StudentProfilesController < ApplicationController
+  def show
+  end
+
+  def new
+    @student_profile = StudentProfile.new
+  end
+
+  def create
+    save_profile_and_redirect
+  end
+
+  private
+
+  def profile_params
+   params.require(:student_profile).permit(:name, :native_language, :learning_objectives)
+  end
+
+  def save_profile_and_redirect
+    @profile = StudentProfile.create(profile_params)
+    current_student.student_profile = @profile
+    if @profile.save
+      redirect_to student_profile_path(:id => current_student.id)
+    else
+      render 'new'
+    end
+  end
 end
---
+++ spec/factories/student_profile.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :student_profile do
+    id 1
+    name 'Test'
+    native_language 'French'
+    learning_objectives 'I want to speak english at restaurants'
+  end
+end
