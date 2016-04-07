c6f484c835036dcdea6c1440958c443b6b095bb2
Richard Moss <richard@englishtutorslive.com>
2016-04-07 14:35:46 +0100

    adds one passing and one pending student profiles controller test

--- spec/controllers/student_profiles_controller_spec.rb
+++ spec/controllers/student_profiles_controller_spec.rb
@@ -1,4 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe StudentProfilesController, type: :controller do
+  describe 'GET new' do
+    xit 'assigns @student_profile' do
+      student_profile = StudentProfile.new
+      get :new
+      expect(assigns(:student_profile)).to eq([student_profile])
+    end
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
 end
