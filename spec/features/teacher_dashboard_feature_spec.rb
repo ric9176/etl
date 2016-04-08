c5a08b7f8a04968bf94ec9e3cb8f358a16ef1295
charlieperson <charlie.person@hyperisland.se>
2016-04-08 23:14:57 +0100

    Creates teacher_model_spec with tests that cause rspec to throw uninitialized constant Teacher

--- spec/features/teacher_dashboard_feature_spec.rb
+++ spec/features/teacher_dashboard_feature_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 feature 'teacher_dashboards' do
-  context 'new student signs up' do
-    scenario 'teachers see a welcome message and a link to view their profile upon sign up' do
+  context 'new teacher signs up' do
+    scenario 'teachers can log in, and a link to view their profile upon sign up' do
       teacher_sign_up
       expect(current_path).to eq '/dashboard'
       expect(page).to have_link 'My profile'
--- spec/models/teacher_model_spec.rb
+++ spec/models/teacher_model_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-feature 'teacher profiles' do
-  
+describe Teacher, type: :model do
+  it "Teacher class implemented" do
+    expect(class_exists?("Teacher"))
+    expect(Teacher < ActiveRecord::Base).to eq(true)
+  end
+  context "Teacher class properties defined" do
+    subject(:teacher) { Teacher.new }
+    it { should have_db_column(:email) }
+    it { should have_db_column(:encrypted_password) }
+    it { should have_db_column(:created_at) }
+    it { should have_db_column(:updated_at) }
+  end
 end
