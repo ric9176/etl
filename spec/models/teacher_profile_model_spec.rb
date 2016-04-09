7827ec1c534a00fd123d75ca7dbfa3dbe4fb7840
charlieperson <charlie.person@hyperisland.se>
2016-04-10 00:53:44 +0100

    Adds failing model tests for TeacherProfiles that cause an uninitialized constant TeacherProfile error

--- spec/models/teacher_profile_model_spec.rb
+++ spec/models/teacher_profile_model_spec.rb
@@ -1,23 +1,23 @@
-# require 'rails_helper'
-#
-# describe TeacherProfile, type: :model do
-#   it 'TeacherProfile class implemented' do
-#     expect(class_exists?('TeacherProfile'))
-#     expect(TeacherProfile < ActiveRecord::Base).to eq(true)
-#   end
-#   context 'TeacherProfile class properties defined' do
-#     subject(:teacher) { TeacherProfile.new }
-#     it { is_expected.to respond_to(:name) }
-#     it { is_expected.to respond_to(:bio) }
-#   end
-#   context 'Teacher profile validations' do
-#     it { should validate_presence_of(:name) }
-#     it { should validate_presence_of(:bio) }
-#   end
-#
-#   context "TeacherProfile associations" do
-#     context "TeacherProfile 1:1 belongs_to relationship to Teacher in place" do
-#       it { should belong_to(:teacher) }
-#     end
-#   end
-# end
+require 'rails_helper'
+
+describe TeacherProfile, type: :model do
+  it 'TeacherProfile class implemented' do
+    expect(class_exists?('TeacherProfile'))
+    expect(TeacherProfile < ActiveRecord::Base).to eq(true)
+  end
+  context 'TeacherProfile class properties defined' do
+    subject(:teacher) { TeacherProfile.new }
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:bio) }
+  end
+  context 'Teacher profile validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:bio) }
+  end
+
+  context "TeacherProfile associations" do
+    context "TeacherProfile 1:1 belongs_to relationship to Teacher in place" do
+      it { should belong_to(:teacher) }
+    end
+  end
+end
