79f3924204234bb24f3e8958a5cdd1893114d624
charlieperson <charlie.person@hyperisland.se>
2016-04-10 11:12:47 +0100

    Passes basic feature tests that allow a teacher to create a profile

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -6,4 +6,20 @@ class TeacherProfilesController < ApplicationController
   def new
     @teacher_profile = TeacherProfile.new
   end
+
+  def create
+    @teacher_profile = TeacherProfile.create(profile_params)
+    if @teacher_profile.save
+      current_teacher.teacher_profile = @teacher_profile
+      redirect_to teacher_profile_path(id: current_teacher.id)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def profile_params
+   params.require(:teacher_profile).permit(:name, :bio)
+  end
 end
