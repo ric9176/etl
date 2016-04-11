de9e5e51b33bb44196a141d8958d7243b4a4c59f
charlieperson <charlie.person@hyperisland.se>
2016-04-11 10:55:11 +0100

    Adds failing model tests that check if teachers and students have a habtm relationship established

--- spec/features/teacher_profile_feature_spec.rb
+++ spec/features/teacher_profile_feature_spec.rb
@@ -28,9 +28,6 @@ feature 'teacher_profiles' do
       end
     end
   end
-
-
-
   context 'teacher is already signed up' do
     before do
       teacher_sign_up
@@ -39,7 +36,7 @@ feature 'teacher_profiles' do
     scenario 'teachers are directed to their specific profile page' do
       teacher_log_out_then_sign_in
       click_link 'My profile'
-      expect(current_path).to eq '/teacher_profiles/5'
+      expect(current_path).to eq '/teacher_profiles/6'
     end
 
     scenario 'teacher can view their profile' do
