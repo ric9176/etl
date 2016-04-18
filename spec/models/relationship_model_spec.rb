132710bbad990abcea7f5a21cc18b49a3e416d8a
charlieperson <charlie.person@hyperisland.se>
2016-04-18 12:11:47 +0100

    Adds Relationship model tests checking that appropriate columns are in place

--- spec/models/relationship_model_spec.rb
+++ spec/models/relationship_model_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe Relationship, type: :model do
-
+  context "Relationship class properties defined" do
+    subject(:relationship) { Relationship.new }
+    it { should have_db_column(:request_status) }
+    it { should have_db_column(:teacher_id) }
+    it { should have_db_column(:student_id) }
+  end
 end
