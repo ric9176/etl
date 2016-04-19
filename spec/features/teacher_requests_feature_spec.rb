9b41b57b1926a9618a09891ed2bb1382f17f4473
charlieperson <charlie.person@hyperisland.se>
2016-04-19 12:51:14 +0100

    Passes test that ensures that a teacher's current students render on their dashboard

--- spec/features/teacher_requests_feature_spec.rb
+++ spec/features/teacher_requests_feature_spec.rb
@@ -10,7 +10,6 @@ feature 'teacher requests' do
     make_student_profile
     click_link 'Find a teacher'
     click_link 'Test teacher'
-    binding.pry
     expect(page).to have_content('Test teacher has not confirmed your request yet')
   end
 
@@ -19,9 +18,7 @@ feature 'teacher requests' do
     teacher_profile = TeacherProfile.create(name: 'Teacher Johnson', bio: 'I rock out')
     teacher.teacher_profile = teacher_profile
     student = Student.create(email: 'student@test.com', password: 'test12345')
-    relationship = Relationship.create
-    relationship.student = student
-    relationship.teacher = teacher
+    relationship = Relationship.create(student_id: student.id, teacher_id: teacher.id)
     student_sign_in('student@test.com', 'test12345')
     make_student_profile
     click_link 'Find a teacher'
@@ -29,6 +26,17 @@ feature 'teacher requests' do
     click_link 'Log out'
     teacher_sign_in('teacher@test.com', 'test12345')
     expect(page).to have_content('Pending requests:')
+    expect(page).to have_content('Test student')
+  end
+
+  scenario 'teachers can see current students on their dashboard' do
+    teacher = Teacher.create(email: 'teacher@test.com', password: 'test12345')
+    student = Student.create(email: 'student@test.com', password: 'test12345')
+    student_profile = StudentProfile.create(name: 'Test student', native_language: 'I rock out', learning_objectives: 'Learn english')
+    student.student_profile = student_profile
+    relationship = Relationship.create(request_status: true, student_id: student.id, teacher_id: teacher.id)
+    teacher_sign_in('teacher@test.com', 'test12345')
+    expect(page).to have_content('Current students:')
     expect(page).to have_content('Test student')
   end
 end
