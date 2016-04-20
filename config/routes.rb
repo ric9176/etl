99231a4b02cbf1028e4157bf3cc9e5e3deada854
Richard Moss <richard@englishtutorslive.com>
2016-04-20 23:12:53 +0200

    refactors routes to remove unused paths and adds user auth to dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :students
   resources :student_profiles
   resources :teacher_profiles
-  resources :relationships
+  resources :relationships, only: [:create, :update, :destroy]
 
 
   root "static_pages#home"
