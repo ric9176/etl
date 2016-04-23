c86cd4c5f24c9ea44aed624ce44c482c0f9936b8
Richard Moss <richard@englishtutorslive.com>
2016-04-23 18:38:10 +0100

    materials controller passing in teacher_id

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,21 +1,32 @@
 class MaterialsController < ApplicationController
+
   def new
-    @class = Material.new(student_id: params[:student_id])
+    @class = Material.new
   end
 
   def create
-    @class = Material.create(class_params)
+    @teacher = current_teacher
+    @class = Material.create(class_params.merge(teacher_id: current_teacher.id))
+    binding.pry
     if @class.save
-      @class.teacher = current_teacher
+      @class.teacher_id = @teacher.id
       redirect_to '/dashboard'
     else
       render 'new'
     end
   end
 
+
+  def update
+    @teacher = current_teacher
+    @class.teacher_id = @teacher.id
+  end
+
+
+
 private
 
   def class_params
-   params.require(:material).permit(:date, :link)
+   params.require(:material).permit(:date, :link, :student_id)
   end
 end
