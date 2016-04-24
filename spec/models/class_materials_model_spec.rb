1ee95b6bb6ceec48bab983c3eeaf658cebbeae94
charlieperson <charlie.person@hyperisland.se>
2016-04-24 14:50:46 +0100

    Adds model tests to check for appropriate columns

--- spec/models/class_materials_model_spec.rb
+++ spec/models/class_materials_model_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe ClassMaterial, type: :model do
-
+  context "ClassMaterial class properties defined" do
+    subject(:class_material) { ClassMaterial.new }
+    it { should have_db_column(:link) }
+    it { should have_db_column(:date) }
+    it { should have_db_column(:student_id) }
+    it { should have_db_column(:teacher_id) }
+  end
 end
