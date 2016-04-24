b01de38e44781a2676cf416d28da70c39df54f1c
charlieperson <charlie.person@hyperisland.se>
2016-04-24 15:07:25 +0100

    Adds create action in classmaterials controller

--- app/controllers/class_materials_controller.rb
+++ app/controllers/class_materials_controller.rb
@@ -5,4 +5,21 @@ class ClassMaterialsController < ApplicationController
     @student_id = params[:student_id]
     session[:student_id] = @student_id
   end
+
+  def create
+    @student_id = session[:student_id]
+    @teacher = current_teacher
+    @class = ClassMaterial.create(class_params.merge(student_id: @student_id, teacher_id: current_teacher.id))
+    if @class.save
+      redirect_to '/dashboard'
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def class_params
+   params.require(:class_material).permit(:date, :link)
+  end
 end
