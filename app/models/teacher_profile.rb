0c661d1f2d621b033b2f3d6c1e60cd9d5bd9ac27
charlieperson <charlie.person@hyperisland.se>
2016-04-28 20:26:29 +0100

    Adds capability for teachers to upload pictures to their profiles

--- app/models/teacher_profile.rb
+++ app/models/teacher_profile.rb
@@ -3,4 +3,5 @@ class TeacherProfile < ActiveRecord::Base
 
   validates :name, :bio, presence: true
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
