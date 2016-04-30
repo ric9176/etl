509f6f6bd0081d3e2f827002e694b2a66cdf806c
charlieperson <charlie.person@hyperisland.se>
2016-04-30 16:40:39 +0100

    Adds failing feature test that checks that a teacher cannot accept a student that already has a teacher

--- spec/features/teacher_requests_feature_spec.rb
+++ spec/features/teacher_requests_feature_spec.rb
@@ -89,4 +89,16 @@ feature 'teacher requests' do
     click_link 'Test teacher'
     expect(page).to have_content('You have already requested this teacher')
   end
+
+  scenario 'teachers should not be able to accept students that already have teachers' do
+    teacher = Teacher.create(email: 'teacher@test.com', password: 'test12345')
+    student = Student.create(email: 'student@test.com', password: 'test12345')
+    student_profile = StudentProfile.create(name: 'Test student', native_language: 'I rock out', learning_objectives: 'Learn english')
+    student.student_profile = student_profile
+    false_relationship = Relationship.create(student_id: student.id, teacher_id: teacher.id)
+    true_relationship = Relationship.create(student_id: student.id, request_status: true)
+    teacher_sign_in('teacher@test.com', 'test12345')
+    click_link 'Confirm'
+    expect(page).to have_content('Sorry, another teacher took on this student before you.')
+  end
 end
