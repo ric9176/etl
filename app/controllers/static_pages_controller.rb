e0de3b9a7d0279d3c783a733f243d24ccc10c88b
charlieperson <charlie.person@hyperisland.se>
2016-05-05 17:49:51 +0100

    Adds sign in functionality in modal window

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,6 @@ class StaticPagesController < ApplicationController
 
   def index
   end
+
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def resource_name
+    :student
+  end
+
+  def resource
+    @resource ||= Student.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:student]
+  end
 end
