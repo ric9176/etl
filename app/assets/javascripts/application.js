f5bf237cd9ef9e412efee68ec5ca479660d7189b
“DovileSand” <dovile.sand@protonmail.ch>
2016-05-07 16:59:12 +0100

    Fades out navbar when scolled down or navigated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,10 @@
 
 var fadeInOnScroll = function(position, element, speed){
   $(document).ready(function() {
-    /* Every time the window is scrolled ... */
     $(window).scroll( function(){
-      /* Check the location of each desired element */
       $(position).each( function(i){
         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
         var bottom_of_window = $(window).scrollTop() + $(window).height();
-        /* If the object is completely visible in the window, fade it it */
         if( bottom_of_window > bottom_of_object ){
           $(element).animate({'opacity':'1'},speed);
         }
@@ -41,15 +38,16 @@ $(document).ready(function() {
   fadeInOnScroll('.pricing-rate', '.table_three', 500);
 });
 
-var documentElem = $(document),
-    nav = $('nav'),
-    lastScrollTop = 0;
-
-  documentElem.on('scroll', function() {
-    var currentScrollTop = $(this).scrollTop();
-
-    if(currentScrollTop > lastScrollTop) nav.addClass('hidden');
-    else nav.removeClass('hidden');
-
-    lastScrollTop = currentScrollTop;
+(function ($) {
+  $(document).ready(function(){
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('.navbar').fadeOut("linear");
+			} else {
+				$('.navbar').fadeIn("ease-in-out");
+			}
+		});
+	});
 });
+  }(jQuery));
