1e8994b79d8083572c1fbe041865d653c47d8612
charlieperson <charlie.person@hyperisland.se>
2016-05-09 02:10:37 +0100

    Platform styling improved

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -1,3 +1,21 @@
+var fadeInOnScroll = function(position, element, speed){
+  $(window).scroll( function(){
+    $(position).each( function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if( bottom_of_window > bottom_of_object ){
+        $(element).animate({'opacity':'1'},speed);
+      }
+    });
+  });
+};
+
+(function ($) {
+  $(document).ready(function() {
+    fadeInOnScroll('.pricing-rate', '.pricing-table', 500);
+  });
+}(jQuery));
+
 (function ($) {
   $(document).ready(function(){
 	$(function () {
