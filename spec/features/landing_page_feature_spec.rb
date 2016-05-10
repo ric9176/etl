d83b99c74f21c6f52d700a081d02b06850a6219a
“DovileSand” <dovile.sand@protonmail.ch>
2016-05-10 19:02:22 +0100

    Renames landing_page feature spec file

--- spec/features/landing_page_feature_spec.rb
+++ spec/features/landing_page_feature_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-feature 'static_pages' do
+feature 'landing_page' do
   context 'first time student visits site' do
     scenario 'should show information about ETL and have navagation bar links' do
       visit '/'
-      expect(page).to have_content 'Can You Really Improve Your English From Home?'
+      expect(page).to have_content 'Our range of services will help you learn English on your terms, focusing on your specific needs'
       expect(page).to have_link 'Why Us'
       expect(page).to have_link 'Services'
       expect(page).to have_link 'Pricing'
